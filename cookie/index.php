38f2303e378a48e91a30a303b692ed097c400796
dheri-boiz <152315705+dheri-boiz@users.noreply.github.com>
2023-11-29 11:34:54 +0530

    Add files via upload

---
+++ cookie/index.php
@@ -0,0 +1,90 @@
+<?php
+// Database connection details
+$host = 'localhost';
+$db = 'user_auth';
+$user = 'root';
+$pass = '';
+
+// Connect to the database
+$conn = new mysqli($host, $user, $pass, $db, 4306);
+
+// Check the connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+// Registration logic
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $stmt = $conn->prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+    $stmt->bind_param('ss', $username, $password);
+    
+    if ($stmt->execute()) {
+        echo 'Registration successful! Please log in.';
+    } else {
+        echo 'Registration failed.';
+    }
+
+    $stmt->close();
+}
+
+// Login logic
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare('SELECT id, password FROM users WHERE username = ?');
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $stmt->bind_result($user_id, $hashed_password);
+
+    if ($stmt->fetch() && password_verify($password, $hashed_password)) {
+        // Set a cookie to store the user's session
+        setcookie('user_id', $user_id, time() + 3600, '/'); // Cookie expires in 1 hour
+        header('Location: dashboard.php');
+    } else {
+        echo 'Login failed. Please check your username and password.';
+    }
+
+    $stmt->close();
+}
+
+// Close the database connection
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Authentication</title>
+</head>
+<body>
+
+<h2>Registration</h2>
+<form method="post" action="">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" required>
+
+    <button type="submit" name="register">Register</button>
+</form>
+
+<h2>Login</h2>
+<form method="post" action="">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" required>
+
+    <button type="submit" name="login">Login</button>
+</form>
+
+</body>
+</html>
